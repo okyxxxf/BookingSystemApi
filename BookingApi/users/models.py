d184363c6b8f85ebef727c017435d27d365a8010
Alexander Bachinskiy <okyxxxf@gmail.com>
2023-09-11 23:09:25 +0300

    feat: create model USer

--- BookingApi/users/models.py
+++ BookingApi/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User (models.Model):
+    name = models.CharField(max_length=20)
+    mail = models.EmailField()
+    password = models.CharField(max_length=20)
+    role = models.CharField(max_length=5)
