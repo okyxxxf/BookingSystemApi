d1c3c80bfe1fecad23d5465e4045aca4347ec23f
Alexander Bachinskiy <okyxxxf@gmail.com>
2023-09-12 12:10:09 +0300

    feat: create userList breakpoint with get and post method of request

---
+++ BookingApi/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from users import views
+
+
+urlpatterns = [
+    path('', views.users_list),
+    #path('<int:pk>', views.)
+]
--- BookingApi/users/views.py
+++ BookingApi/users/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import generics
+from users.models import User
+from users.serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def users_list(request):
+    users = User.objects.all()
+
+    if request.method == 'GET':
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
