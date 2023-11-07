6d7bd54c37e35a9b39f436fd6100dc54c853e010
Alexander Bachinskiy <okyxxxf@gmail.com>
2023-11-07 17:25:51 +0300

    refactor: refactor get_concert func

--- BookingApi/concerts/views/getConcert.py
+++ BookingApi/concerts/views/getConcert.py
@@ -3,11 +3,13 @@ from django.db.models import Model
 from concerts.serializers import ConcertSerializer
 
 def get_concert (
-        serializer : ModelSerializer, 
-        Model : Model, 
-        concert : Model
-    ):
+    serializer : ModelSerializer, 
+    Model : Model, 
+    concert : Model
+):
+    
     model_concert = Model.objects.get(concert_id = concert.id)
+
     return [
         ConcertSerializer(concert).data, 
         serializer(model_concert).data
