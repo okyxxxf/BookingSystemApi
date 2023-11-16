d718bcf6f4a020173fbf1b625ccfc8821ef2a23d
Alexander Bachinskiy <okyxxxf@gmail.com>
2023-11-16 23:56:27 +0300

    feat: add functionality to change ticket count on booking

--- BookingApi/transactions/views/transactions_list.py
+++ BookingApi/transactions/views/transactions_list.py
@@ -3,6 +3,7 @@ from transactions.models import Transaction
 from transactions.serializers import TransactionSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from concerts.models import Concert
 
 @api_view(['GET', 'POST'])
 def transactions_list(request):
@@ -16,7 +17,12 @@ def transactions_list(request):
             
         case 'POST':
             serializer = TransactionSerializer(data=request.data)
-            
+            concert = Concert.objects.get(id=request.data['concert_id'])
+            if concert.tickets_count < 1: 
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            concert.tickets_count -= 1
+            concert.save()   
+
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
