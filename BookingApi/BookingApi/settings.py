b6050ba01004414a0838b544d1ff75bfdcb40219
Alexander Bachinskiy <okyxxxf@gmail.com>
2023-12-12 10:15:53 +0300

    feat: build app

--- BookingApi/BookingApi/settings.py
+++ BookingApi/BookingApi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t#+klg+19nn94pnz&p51r0gkmg6y=6df3)ab4pv4+zbzecu%04'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
